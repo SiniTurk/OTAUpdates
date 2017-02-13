63e9d436e77953dd1244d8ac15a2737c2cc3d94f
Tim Schumacher <timschumi2@arcor.de>
2017-02-13 21:09:40 +0100

    OTAUpdates: Various changes
    - Cleanup
    - Fixed some License headers
    - Moved some hardcoded strings to strings.xml

--- app/src/main/java/berkantkz/otaupdates/Settings.java
+++ app/src/main/java/berkantkz/otaupdates/Settings.java
@@ -1,8 +1,25 @@
-package berkantkz.otaupdates;
-
 /**
- * Created by berka on 12.02.2017.
+ * Project: OTAUpdates
+ *
+ * @author berkantkz, TimSchumi
+ * License: GNU General Public License, Version 3
+ * <p>
+ * Copyright 2017 Berkant Korkmaz, Tim Schumacher
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+package berkantkz.otaupdates;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
