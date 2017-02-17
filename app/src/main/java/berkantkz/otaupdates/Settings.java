52787ea91709304ce6981e23a17a3b3f428c8665
Berkant Korkmaz <berkantk3@gmail.com>
2017-02-17 21:47:28 +0300

    Add force English language option

--- app/src/main/java/berkantkz/otaupdates/Settings.java
+++ app/src/main/java/berkantkz/otaupdates/Settings.java
@@ -21,13 +21,19 @@
 
 package berkantkz.otaupdates;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.support.v7.app.AlertDialog;
 
 import eu.chainfire.libsuperuser.Shell;
 
 public class Settings extends PreferenceActivity {
+
+    public static Activity SettingsActivity;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,11 +41,41 @@ public class Settings extends PreferenceActivity {
 
         addPreferencesFromResource(R.xml.preferences);
 
+        SettingsActivity = Settings.this;
+
         CheckBoxPreference enable_auto_install = (CheckBoxPreference) findPreference("enable_auto_install");
         if (Shell.SU.available() == false) {
             enable_auto_install.setEnabled(false);
             enable_auto_install.setSummary("Only rooted/root granted devices are supported");
         }
 
+        CheckBoxPreference setEnglish = (CheckBoxPreference) findPreference("force_english");
+        setEnglish.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                force_english_dialog();
+                return true;
+            }
+        });
+
     }
+
+    public void force_english_dialog() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Force English");
+                builder.setMessage("Relaunch is required to take affect, you can relaunch now or later. Would you like to restart now?");
+                builder.setPositiveButton(getString(R.string.button_yes), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        android.os.Process.killProcess(android.os.Process.myPid());
+                    }
+                });
+                builder.setNegativeButton(getString(R.string.button_no), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.setCancelable(true);
+                alert.show();
+            }
+
 }
